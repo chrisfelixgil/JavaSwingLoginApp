960315e6b2d07286f4d627d643fd2fff39ad8439
Chris Castle <christiangil2705@hotmail.com>
2024-07-31 00:18:59 -0400

    Primera etapa para la conexion a base de datos

--- src/Modelos/dbConecction.java
+++ src/Modelos/dbConecction.java
@@ -1,11 +1,24 @@
 
 package Modelos;
 
+import java.sql.Connection;
+
 /**
  *
  * @author chris
  */
 public class dbConecction {
+    
+    String dbConecction = "";
+    Connection conn = null;
+    
+    public dbConecction(){
+        try {
+            
+        } catch (Exception e) {
+        }
+        
+    }
     
     
     
